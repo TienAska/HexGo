3b9ff6f175a5a70397b71bf7b3ebea22be8b91d4
TienAska <constantine.sky@hotmail.com>
2022-04-15 15:42:01 +0800

    SnapeToGrid return the hex coordinates.

--- Source/HexGo/Public/Hex/HexBlueprintLibrary.h
+++ Source/HexGo/Public/Hex/HexBlueprintLibrary.h
@@ -6,23 +6,28 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "HexBlueprintLibrary.generated.h"
 
+USTRUCT(BlueprintType)
 struct FHex
 {
+	GENERATED_BODY()
+
 public:
+	FHex() {}
+
 	FHex(float x, float y, float size) :
-		q((FGenericPlatformMath::Sqrt(3) / 3 * y - 1.0 / 3 * x) / size), r((2.0 / 3 * x) / size), size(size)
+		q((FMath::Sqrt(3) / 3 * y - 1.0 / 3 * x) / size), r((2.0 / 3 * x) / size), size(size)
 	{
 	}
 
 	FHex& Round()
 	{
-		float _q = round(q);
-		float _r = round(r);
-		float _s = round(-q - r);
+		float _q = FMath::RoundToFloat(q);
+		float _r = FMath::RoundToFloat(r);
+		float _s = FMath::RoundToFloat(-q - r);
 
-		float q_diff = abs(_q - q);
-		float r_diff = abs(_r - r);
-		float s_diff = abs(_s - (-q - r));
+		float q_diff = FMath::Abs(_q - q);
+		float r_diff = FMath::Abs(_r - r);
+		float s_diff = FMath::Abs(_s - (-q - r));
 
 		if (q_diff > r_diff && q_diff > s_diff)
 			_q = -_r - _s;
@@ -44,8 +49,12 @@ public:
 		return FVector(x, y, 0);
 	}
 
-private:
-	float q, r, size;
+	UPROPERTY(BlueprintReadOnly)
+	float q;
+	UPROPERTY(BlueprintReadOnly)
+	float r;
+	UPROPERTY(BlueprintReadOnly)
+	float size;
 };
 
 
@@ -58,9 +67,9 @@ class HEXGO_API UHexBlueprintLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 	
 	UFUNCTION(BlueprintCallable, Category="Hex")
-	static FVector SnapeToGrid(const FVector& worldPos, float size)
+	static FVector SnapeToGrid(const FVector& worldPos, float size, FHex& hex)
 	{
-		FHex hex(worldPos.X, worldPos.Y, size);
+		hex = FHex(worldPos.X, worldPos.Y, size);
 		return hex.Round().Position();
 	}
 };
